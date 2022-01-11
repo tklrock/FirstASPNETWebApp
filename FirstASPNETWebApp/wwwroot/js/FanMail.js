fb5d8e204862aacc5b017331d4f4c9cac42bc491
tklrock <83993369+tklrock@users.noreply.github.com>
2022-01-10 23:28:05 -0700

    Add project files.

---
+++ FirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿/*
+document.getElementById("buttonSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("textFrom").value + "\nSubject: " + document.getElementById("textSubject").value + "\nMessage: " + document.getElementById("textMessage").value);
+})*/
+
+// jquery version
+
+$("#buttonSend").click(function () {
+    alert("From: " + $("#textFrom").val() + "\nSubject: " + $("#textSubject").val() + "\nMessage: " + $("#textMessage").val());
+
+    //Fun animation
+    $("#picGanderson").fadeOut("slow");
+})
